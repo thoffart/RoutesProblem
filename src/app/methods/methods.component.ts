daeabb2381fcb78738eb1cdc754269b53a6a3da8
vvas <vinedias@gmail.com>
2018-03-28 20:20:42 -0300

    nous commençons le construction des methodes

--- src/app/methods/methods.component.ts
+++ src/app/methods/methods.component.ts
@@ -19,13 +19,13 @@ export class MethodsComponent implements OnInit {
   resolve(): void {
     //this.mapgetroutesservice.getarrays(this.distance, this.duration);
     //class example
-    this.distance[0] = [1000, 2, 4, 1, 10, 1000, 1000];
-    this.distance[1] = [2, 1000, 1000, 1, 1000, 1000, 1000];
-    this.distance[2] = [4, 1000, 1000, 4, 1000, 1000, 3];
-    this.distance[3] = [1, 1, 4, 1000, 7, 10, 7];
-    this.distance[4] = [10, 1000, 1000, 7, 1000, 8, 5];
-    this.distance[5] = [1000, 1000, 1000, 10, 8, 1000, 3];
-    this.distance[6] = [1000, 1000, 3, 7, 5, 3, 1000];
+    this.distance[0] = [1000,     2,   4,   1,    10,   1000,   1000];
+    this.distance[1] = [    2, 1000,1000,   1,   1000,  1000,   1000];
+    this.distance[2] = [    4, 1000,1000,   4,   1000,  1000,      3];
+    this.distance[3] = [    1,    1,   4,1000,      7,    10,      7];
+    this.distance[4] = [   10, 1000,1000,   7,   1000,     8,      5];
+    this.distance[5] = [ 1000, 1000,1000,  10,      8,  1000,      3];
+    this.distance[6] = [ 1000, 1000,   3,   7,      5,     3,   1000];
     this.extmin();
   }
 
@@ -40,6 +40,7 @@ export class MethodsComponent implements OnInit {
           marcano[0] = i;
           marcano[1] = j;
         }
+        
       }
     }
   }
