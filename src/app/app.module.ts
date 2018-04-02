b0328644ad501ccf18cda7733b77c9b1d27469a4
vvas <vinedias@gmail.com>
2018-04-02 11:46:20 -0300

    atualizando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { AppRoutingModule } from './sharedModules/app-routing.module';
 import { AuthGuardService } from './login/auth-guard.service';
 import { AuthService } from './login/auth.service';
 import { FormstoroutesService } from './sharedservices/formstoroutes.service';
-import { MapainicioModule } from './mapainicio/mapainicio.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -12,17 +11,12 @@ import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
 import { RegisterModule } from './register/register.module';
 import { HomeModule } from './home/home.module';
-import { FormsmapsModule } from './formsmaps/formsmaps.module';
 import { MapgetroutesService } from './sharedservices/mapgetroutes.service';
-import { MethodsComponent } from './methods/methods.component';
-import { ExemplosComponent } from './exemplos/exemplos.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MethodsComponent,
-    ExemplosComponent,
 ],
   imports: [
     LoginModule,
@@ -30,8 +24,6 @@ import { ExemplosComponent } from './exemplos/exemplos.component';
     HomeModule,
     AppRoutingModule,
     BrowserModule,
-    MapainicioModule,
-    FormsmapsModule,
   ],
   providers: [
     AuthService, 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,21 @@ import { AppRoutingModule } from './../sharedModules/app-routing.module';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MapainicioModule } from '../mapainicio/mapainicio.module';
+import { FormsmapsModule } from '../formsmaps/formsmaps.module';
+import { MethodsComponent } from '../methods/methods.component';
+import { ExemplosComponent } from '../exemplos/exemplos.component';
 @NgModule({
     declarations: [
         HomeComponent,
+        MethodsComponent,
+        ExemplosComponent,
     ],
     imports: [
         CommonModule,
-        AppRoutingModule
+        AppRoutingModule,
+        MapainicioModule,
+        FormsmapsModule,
     ]
 })
 
