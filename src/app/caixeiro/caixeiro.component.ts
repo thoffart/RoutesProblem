c19b2488358f88a7512cb798fc6a5816f1be64c1
denisakira <denis_akira@hotmail.com>
2018-04-07 20:02:57 -0300

    eliminando zeros da matriz

--- src/app/caixeiro/caixeiro.component.ts
+++ src/app/caixeiro/caixeiro.component.ts
@@ -8,9 +8,59 @@ import { Component, OnInit } from '@angular/core';
 export class CaixeiroComponent implements OnInit {
 
   x: number[][] = [];
-  y: number[] = [];
-  z: number[] = [];
+  y: number[][] = [];
+  z: number[][] = [];
   w: number[] = [];
+  estado: string[] = [];
+
+  moment(matrix) {
+    var aux = matrix.map(function(arr) {
+      return arr.slice();
+    });
+
+    return aux;
+
+  }
+  metodo(matrix) {
+    this.x = matrix;
+
+    this.x = this.subtraiLinhas(matrix);
+    this.y = this.moment(matrix);
+
+
+    this.x = this.subtraiColunas(matrix);
+
+    let obj = [];
+
+
+
+    obj = this.encontraZeros(matrix);
+
+    let posCol = obj.pop();
+    let posLin = obj.pop();
+
+
+    matrix.splice(posLin,1);
+
+    for (let i in matrix) {
+      matrix[i].splice(posCol,1);
+    }
+
+
+
+
+    this.encontraZeros(matrix);
+
+    posCol = obj.pop();
+    posLin = obj.pop();
+
+    matrix.splice(posLin,1);
+
+    for (let i in matrix) {
+      matrix[i].splice(posCol,1);
+    }
+
+  }
 
   subtraiLinhas(matrix) {
     let minLin: number[] = [];
@@ -18,13 +68,20 @@ export class CaixeiroComponent implements OnInit {
     //pega os menores valores de todas as linhas
     for (const j in matrix) {
       const w = matrix[j];
+      for ( let i=0; i<w.length; i++) {
+        if(w[i]==0)
+          w.splice(i,1);
+      } 
       minLin.push(Math.min(...w));
     }
 
     //subtrai os valores de minLin nas colunas da matriz
     for (const i in matrix) {
       for (const j in matrix[i]) {
-        matrix[i][j] -= minLin[i]
+        if (matrix[i][j] == 0 )
+          continue;
+        else
+          matrix[i][j] -= minLin[i]
       }
     }
     return matrix;
@@ -38,15 +95,18 @@ export class CaixeiroComponent implements OnInit {
     //aloca esses valores em minCol
     for (const j in matrix) {
       for (const i in matrix[j]) {
-        aux.push(matrix[i][j]); 
+        aux.push(matrix[i][j]);
       }
       minCol.push(Math.min(...aux));
       aux = [];
     }
-    //subtrai da matriz 
+    //subtrai da matriz
     for (const j in matrix) {
       for (const i in matrix[j]) {
-        matrix[i][j] -= minCol[j];
+        if(matrix[i][j]==0)
+          continue;
+        else
+          matrix[i][j] -= minCol[j];
       }
     }
 
@@ -54,7 +114,22 @@ export class CaixeiroComponent implements OnInit {
 
   }
 
-  encontraZeros(matrix) {
+  caminho(matrix,posLin,posCol,zeroLin,zeroCol){
+    var aux = matrix.map(function(arr) {
+      return arr.slice();
+    });
+
+    for (let i of matrix) {
+
+    }
+  }
+
+  contaZeros(matrix) {
+    let contLin: number = 1000;
+    let contCol: number = 1000;
+    let posLin: number = 0;
+    let posCol: number = 0;
+
     //declara as variáveis que contam o número de zeros nas linhas e colunas
     let zeroLin: number[] = [];
     for (const i in matrix) {
@@ -66,42 +141,46 @@ export class CaixeiroComponent implements OnInit {
       zeroCol[j] = 0;
     }
 
-    //variável zeros, que contém a posição dos 0s na matriz
-    let zeros:number[][] = [];
-
-    for (let i=0; i<matrix.length; i++) {
-      for (let j=0;j<matrix[0].length; j++) {
-        zeros[i][j] = 0;
-      }
-    }
-    
+    //encontra a quantidade de zeros nas colunas e linhas
     for (const i in matrix) {
-      for (const j in matrix[0]) {
-        if (matrix[i][j] == 0) {
-          zeros[i][j] = 1;
-        }
-        else {
-          zeros[i][j] = 0;
+      for (const j in matrix[i]) {
+        if (matrix[i][j]==0) {
+          return false;
         }
       }
     }
 
+    return true;
+  }
+
+  encontraZeros(matrix) {
     let contLin: number = 1000;
     let contCol: number = 1000;
     let posLin: number = 0;
     let posCol: number = 0;
-    
+
+    //declara as variáveis que contam o número de zeros nas linhas e colunas
+    let zeroLin: number[] = [];
+    for (const i in matrix) {
+      zeroLin[i] = 0;
+    }
+
+    let zeroCol: number[] = [];
+    for(const j in matrix[0]) {
+      zeroCol[j] = 0;
+    }
+
     //encontra a quantidade de zeros nas colunas e linhas
     for (const i in matrix) {
       for (const j in matrix[i]) {
         if (matrix[i][j]==0) {
           zeroLin[i] += 1;
-          zeroCol[j] += 1;          
+          zeroCol[j] += 1;
         }
       }
     }
 
-    //encontra as colunas e linhas com menor número de zeros
+    //encontra a coluna e linha com menor número de zeros
     for (let i=0; i<zeroLin.length; i++) {
       if (contLin > zeroLin[i]) {
         contLin = zeroLin[i];
@@ -110,11 +189,12 @@ export class CaixeiroComponent implements OnInit {
     }
 
     for (let i=0; i<zeroCol.length; i++) {
-      if (contCol > zeroCol[i]) {
-        contCol = zeroCol[i];
+      if(matrix[posLin][i]==0) {
         posCol = i;
       }
     }
+
+    //this.caminho(matrix,posLin,posCol,zeroLin,zeroCol);
 
     let obj = [];
     obj.push(posLin, posCol);
@@ -130,20 +210,11 @@ export class CaixeiroComponent implements OnInit {
     matrix = [[1000, 65, 53, 37], [65, 1000, 95, 1000],
               [53, 95, 1000, 81], [37, 1000, 81, 1000]];
 
-    this.x = matrix;
 
-    this.x = this.subtraiLinhas(matrix);
+    this.z = this.moment(matrix);
+          
 
-    this.x = this.subtraiColunas(matrix);
-
-    let obj = [];
-
-    obj = this.encontraZeros(matrix);
-
-    this.y = obj.pop();
-    this.w = obj.pop();
-
-
+    this.metodo(matrix);
   }
 
   ngOnInit() {
