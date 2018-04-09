1f4c72f81c5f1e4756885da6bf8f52540819d994
thoffart <thiagohoffart321920@gmail.com>
2018-04-08 21:19:26 -0300

    tabelas de exemplos criadas

--- src/app/eds/eds.component.ts
+++ src/app/eds/eds.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../home/home.component.css']
 })
 export class EdsComponent implements OnInit {
-
+  itens = [1];
+  metodos = ['Extensão Mínima', 'Percurso Mínimo', 'Fluxo Máximo', 'Caxeiro'];
+  distance: number[][] = [];
   constructor() { }
 
   ngOnInit() {
+    this.distance[0] = [Infinity, 20, 30, 10, Infinity];
+    this.distance[1] = [Infinity, Infinity, 40, Infinity, 30];
+    this.distance[2] = [Infinity, Infinity, Infinity, 10, 20];
+    this.distance[3] = [Infinity, Infinity, 5, Infinity, 20];
+    this.distance[4] = [Infinity, Infinity, Infinity, Infinity, Infinity];
   }
 
 }
